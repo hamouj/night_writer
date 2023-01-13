03a654c06901bd6908f5b6d867dd7a18b8cbd67e
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-13 12:54:10 -0800

    Feat: create #content

--- lib/content_reader.rb
+++ lib/content_reader.rb
@@ -1,11 +1,13 @@
 class ContentReader
   attr_reader :english_text,  
-              :braille_text
+              :braille_text,
+              :content
 
   def initialize(locations)
     @english_text = File.open(locations[:english_text], "r")
     @braille_text = File.open(locations[:braille_text], "w")
     @character_count = @english_text.size
+    @content = IO.readlines(english_text,40)
   end
 
   def confirmation_message
