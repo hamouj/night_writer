18a12cbcb96ba22a65e5002c362a9aebe1880cf3
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 11:07:34 -0800

    Fix: update naming of #split_content and #content

--- lib/content_reader.rb
+++ lib/content_reader.rb
@@ -1,24 +1,24 @@
 class ContentReader
   attr_reader :english_text,  
               :braille_text,
-              :content
+              :english_text_content
 
   def initialize(locations)
     @english_text = File.open(locations[:english_text], "r")
     @braille_text = File.open(locations[:braille_text], "w")
     @english_text_character_count = @english_text.size
-    @content = IO.readlines(@english_text,40)
+    @english_text_content = IO.readlines(@english_text,40)
   end
 
   def night_writer_confirmation_message
     "Created #{ARGV[1]} containing #{@english_text_character_count} characters"
   end
 
-  def split_content
-    split_content = []
-    content.each do |line|
-      split_content << line.split(//)
+  def split_english_text_content
+    english_text_split_content = []
+    english_text_content.each do |line|
+      english_text_split_content << line.split(//)
     end
-    split_content
+    english_text_split_content
   end
 end
