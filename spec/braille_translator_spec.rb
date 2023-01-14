657929477202c38c341e5d49b1913542ef97ba7a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 14:06:56 -0800

    Test: create #initialize test

---
+++ spec/braille_translator_spec.rb
@@ -0,0 +1,22 @@
+require './spec/spec_helper'
+require './lib/braille_translator'
+require './lib/content_reader'
+
+describe BrailleTranslator do 
+  File.open('braille_sample.txt', "w+") {|file| file.write(".00.00000.0...0.0.0..00.0.0....00.00.0\n0.....0.0..0..0.00..0.0.0..0..00.0..00\n0...0.0.0.......0.....0.0.....0...000.\n")}
+
+  let(:braille_translator) {BrailleTranslator.new({
+    :english_text => 'sample.txt',
+    :braille_text => 'braille_sample.txt'
+  })}
+  let(:content_reader) {ContentReader.new({
+    :english_text => 'sample.txt',
+    :braille_text => 'braille_sample.txt'
+  })}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(braille_translator).to be_a(BrailleTranslator)
+    end
+  end
+end
