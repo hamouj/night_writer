f5530b1dfe81c56533a503aab9e45aee3c9fef9c
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 09:12:33 -0800

    Feat: add instance of EnglishTranslator and #translate to runner

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,16 @@
 require './lib/content_reader'
+require './lib/english_translator'
 
 content_reader = ContentReader.new({
   english_text: ARGV[0],
   braille_text: ARGV[1]
 })
+
+english_translator = EnglishTranslator.new({  
+  english_text: ARGV[0],
+  braille_text: ARGV[1]
+})
+
+english_translator.translate
 
 p content_reader.confirmation_message
