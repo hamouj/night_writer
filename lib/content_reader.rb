010bd3fa6d5c41c59f8b694259fd85e12799586d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 10:44:12 -0800

    Fix: add @ to instance_variable english_text

--- lib/content_reader.rb
+++ lib/content_reader.rb
@@ -7,7 +7,7 @@ class ContentReader
     @english_text = File.open(locations[:english_text], "r")
     @braille_text = File.open(locations[:braille_text], "w")
     @character_count = @english_text.size
-    @content = IO.readlines(english_text,40)
+    @content = IO.readlines(@english_text,40)
   end
 
   def confirmation_message
