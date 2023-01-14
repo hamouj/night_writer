8d9651600a2d105676feaf9971955a135efe9666
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 11:04:06 -0800

    Fix: update naming of confirmation message method

--- lib/content_reader.rb
+++ lib/content_reader.rb
@@ -6,12 +6,12 @@ class ContentReader
   def initialize(locations)
     @english_text = File.open(locations[:english_text], "r")
     @braille_text = File.open(locations[:braille_text], "w")
-    @character_count = @english_text.size
+    @english_text_character_count = @english_text.size
     @content = IO.readlines(@english_text,40)
   end
 
-  def confirmation_message
-    "Created #{ARGV[1]} containing #{@character_count} characters"
+  def night_writer_confirmation_message
+    "Created #{ARGV[1]} containing #{@english_text_character_count} characters"
   end
 
   def split_content
