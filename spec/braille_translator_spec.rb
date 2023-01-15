9e1d18090c3147c7bab5be9bdde5f9b15dfdbb7d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 19:48:28 -0800

    Fix: update sample file for tests

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -3,15 +3,17 @@ require './lib/braille_translator'
 require './lib/content_reader'
 
 describe BrailleTranslator do 
-  File.open('braille_sample.txt', "w+") {|file| file.write(".00.00000.0...0.0.0..00.0.0....00.00.0\n0.....0.0..0..0.00..0.0.0..0..00.0..00\n0...0.0.0.......0.....0.0.....0...000.\n")}
+  File.open('braille_text_2.txt', "w+") {|file| file.write(".00.00000.0...0.0.0..00.0.0....00.00.0\n0.....0.0..0..0.00..0.0.0..0..00.0..00\n0...0.0.0.......0.....0.0.....0...000.\n")}
+  braille_text_2 = File.open('braille_text_2.txt', "r")
+  english_text_2 = File.open('english_text_2.txt', "w")
 
   let(:braille_translator) {BrailleTranslator.new({
-    :english_text => 'sample.txt',
-    :braille_text => 'braille_sample.txt'
+    :english_text => english_text_2,
+    :braille_text => braille_text_2
   })}
   let(:content_reader) {ContentReader.new({
-    :english_text => 'sample.txt',
-    :braille_text => 'braille_sample.txt'
+    :english_text => english_text_2,
+    :braille_text => braille_text_2
   })}
 
   describe '#initialize' do
