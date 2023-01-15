7697c1ea2b5da8b9fbfec44a86759bae9f4e05c6
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-14 18:46:21 -0800

    Fix: abstract file_paths

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,6 +1,14 @@
 require './lib/content_reader'
+require './lib/braille_translator'
 
-content_reader = ContentReader.new({
+file_paths = {
   english_text: ARGV[1],
   braille_text: ARGV[0]
-})
+}
+
+content_reader = ContentReader.new(file_paths)
+braille_translator = BrailleTranslator.new(file_paths)
+
+braille_translator.translate
+
+p content_reader.night_reader_confirmation_message
